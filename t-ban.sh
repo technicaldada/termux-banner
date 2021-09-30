00e5bee54ffd12cb9e04127cf1fb4633c310f5db
JayKali <62602243+jaykali@users.noreply.github.com>
2021-09-30 02:37:12 -0400

    Update t-ban.sh

--- t-ban.sh
+++ t-ban.sh
@@ -2,7 +2,7 @@
 #
 #
 # Termux Banner
-# Banner by Technical Dada (KP)
+# Banner by Koushik Pal (KP)
 # _____Do Not Copy Or Modify_____
 #
 #
@@ -12,9 +12,9 @@ cyan='\e[0;36m'
 lightgreen='\e[1;32m'
 red='\e[1;31m'
 yellow='\e[1;33m'
-echo -e $lightgreen "\e[1m                Technicaldada4us "
+echo -e $lightgreen "\e[1m                KoushikPal.Com "
 echo " "
-echo -e $yellow "\e[1m                         -Technnical Dada "
+echo -e $yellow "\e[1m                         -Koushik Pal "
 echo " "
 echo " "
 echo " "
