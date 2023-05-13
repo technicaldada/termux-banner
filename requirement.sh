21f6a59cdbdf56aa111a4a6871254caf6f1f818b
Israel ezekiel <132762280+Israelezekiel@users.noreply.github.com>
2023-05-13 17:37:40 +0100

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -12,6 +12,6 @@ apt install ruby -y
 gem install lolcat
 figlet -f big Done !!! | lolcat
 echo
-echo -e "\e[1m Now Run bash t-ban.sh...!!!"
+echo -e "\e[1m Now Run bash israel.sh...!!!"
 echo
-echo -e "\e[1m\e[32m Developed by :\e[33m Koushik Pal (KP)"
+echo -e "\e[1m\e[32m Developed by :\e[33m Israel-hacks (IK)"
